b39a0389a44e35f430c98564be518442dfc0001e
Njunge Njenga <njungenjenga@Njunges-MacBook-Pro.local>
2019-02-04 19:59:58 +0300

    Add jumbotron

---
+++ data.js
@@ -0,0 +1,107 @@
+export default {
+  hipHopAlbums: [
+    {
+      albumTitle: "A  Love Letter to You 3",
+      artist: "Trippie Redd",
+      image: "trippie.jpg	"
+    },
+    {
+      albumTitle: "I am > i was",
+      artist: "21 Savage",
+      image: "iamiwas.jpg"
+    },
+    {
+      albumTitle: "",
+      artist: "",
+      image: "iop.jpg	"
+    },
+    {
+      albumTitle: "K.O.D",
+      artist: "J Cole",
+      image: "kod.jpg	"
+    },
+    {
+      albumTitle: "Dime Trap",
+      artist: "T.I",
+      image: "dime.jpg"
+    },
+    {
+      albumTitle: "Scorpion",
+      artist: "Darake",
+      image: "scorpion.jpg"
+    },
+    {
+      albumTitle: "Vicory Lap",
+      artist: "Nipsey Hussle",
+      image: "victory.jpg	"
+    },
+    {
+      albumTitle: "Daytona",
+      artist: "Pusha T",
+      image: "daytona.jpg	"
+    },
+    {
+      albumTitle: "Championships",
+      artist: "Meek Mill",
+      image: "championships.jpeg"
+    },
+    {
+      albumTitle: "Astroworld",
+      artist: "Travis Scott",
+      image: "astroworld.jpg"
+    }
+  ],
+
+  rnbAlbums: [
+    {
+      albumTitle: "Oxnard",
+      artist: "Anderson Perk",
+      image: "oxnard.jpg"
+    },
+    {
+      albumTitle: "Dirty Computer",
+      artist: "Janelle Money",
+      image: "dirty_computer.jpeg"
+    },
+    {
+      albumTitle: "My Dear Melancholy",
+      artist: "The Weeknd",
+      image: "my_dear_melancholy.jpg"
+    },
+    {
+      albumTitle: "I Used to Know Her: Part 2",
+      artist: "H.E.R",
+      image: "her.jpg"
+    },
+    {
+      albumTitle: "East Atlanta Love Letter",
+      artist: "6lack",
+      image: "ell.jpg"
+    },
+    {
+      albumTitle: "Dancing Shadows",
+      artist: "Mario",
+      image: "ds.jpeg	"
+    },
+    {
+      albumTitle: "4275",
+      artist: "Jacquees",
+      image: "4275.jpg"
+    },
+    {
+      albumTitle: "Everything is love",
+      artist: "The Carters",
+      image: "everything.jpeg"
+    },
+    {
+      albumTitle: "Sweetener",
+      artist: "Ariana Grande",
+      image: "sweeterner.jpg"
+    },
+    {
+      albumTitle: "Scorpion",
+      artist: "Drake",
+      image: "scorpion.jpg"
+    }
+  ]
+};
