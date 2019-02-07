78e99e78440e75c0797112c83323aa42c85d978e
Njunge Njenga <njungedev@gmail.com>
2019-02-07 14:57:57 +0300

    Add lazy loading logic

--- app.js
+++ app.js
@@ -89,8 +89,36 @@ class LazyloadImages {
   `;
     root.innerHTML = markup;
   }
+  /**
+   * lazyLoadImages
+   * * Use the Intersection Observer API to implement lazy loading.
+   */
+  lazyLoadImages() {
+    document.addEventListener("DOMContentLoaded", function() {
+      let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+      if ("IntersectionObserver" in window) {
+        let lazyImageObserver = new IntersectionObserver(function(entries) {
+          entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+              let lazyImage = entry.target;
+              lazyImage.src = lazyImage.dataset.src;
+              lazyImage.srcset = lazyImage.dataset.srcset;
+              lazyImage.classList.remove("lazy");
+              lazyImageObserver.unobserve(lazyImage);
+            }
+          });
+        });
+        lazyImages.forEach(function(lazyImage) {
+          lazyImageObserver.observe(lazyImage);
+        });
+      } else {
+        // Use fallback to implement lazy loading
+      }
+    });
+  }
 }
 const lazyLoad = new LazyloadImages();
 lazyLoad.renderAlbums("hip-hop");
 lazyLoad.renderAlbums("rnb");
 lazyLoad.animateJumbotronContents();
+lazyLoad.lazyLoadImages();
